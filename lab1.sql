fa9a6abb79c3dc0b3122c81bfd7e639cf89d22e5
Aruzhan <155585792+tleukulova@users.noreply.github.com>
2024-09-17 17:23:35 +0500

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,14 +1,18 @@
+--Task 1
 CREATE DATABASE lab1;
 
+--Task2
 CREATE TABLE users (
     id bigserial primary key,
     firstname varchar(100),
     lastname varchar(100)
     )
 
+--Task3
 ALTER TABLE users
 ADD COLUMN isadmin INTEGER;
 
+--Task4
 ALTER TABLE users
 ALTER COLUMN isadmin TYPE BOOLEAN
 USING CASE
@@ -16,20 +20,26 @@ USING CASE
     WHEN isadmin = 1 THEN true
 END;
 
+--Task5
 ALTER TABLE users
 ALTER COLUMN isadmin SET DEFAULT false;
 
 ALTER TABLE users
 DROP CONSTRAINT IF EXISTS users_pkey;
 
+--Task6
 ALTER TABLE users
 ADD CONSTRAINT pk_users_id PRIMARY KEY (id);
 
+--Task7
 CREATE TABLE tasks (
     id bigserial primary key,
     name varchar(50),
     user_id int
 )
 
+--Task8
 DROP TABLE tasks;
+
+--Task9
 DROP DATABASE lab1;
